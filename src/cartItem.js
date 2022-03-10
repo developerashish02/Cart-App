b067a7656b34740fa7964c2f0e89a3231c3cdf51
Ashish_Amar_Gaikwad <developerashish02@gmail.com>
2022-03-10 18:19:38 +0530

    Adding state in cart item

--- src/cartItem.js
+++ src/cartItem.js
@@ -1,17 +1,49 @@
 import React from "react";
 
 class CartItem extends React.Component {
+	// Adding state to the componets
+	constructor() {
+		super();
+		this.state = {
+			price: 999,
+			title: "Phone",
+			qty: 1,
+			img: "",
+		};
+	}
 	render() {
+		const { price, title, qty } = this.state;
 		return (
 			<div className="cart-item">
 				<div className="left-block">
 					<img style={styles.image} />
 				</div>
 				<div className="right-block">
-					<div style={{ fontSize: 25 }}>Phone</div>
-					<div style={{ color: "#777" }}>Rs 999</div>
-					<div style={{ color: "#777" }}>Qty:1</div>
-					<div className="cart-item-action">{/* Buttons */}</div>
+					<div style={{ fontSize: 25 }}>{title}</div>
+					<div style={{ color: "#777" }}> Rs {price} </div>
+					<div style={{ color: "#777" }}> Qty {qty}</div>
+					<div className="cart-item-actions">
+						{/* Add item to cart  */}
+						<img
+							src="https://cdn-icons-png.flaticon.com/512/1828/1828919.png "
+							alt="Add item "
+							className="action-icons "
+						/>
+
+						{/* remove item to cart  */}
+						<img
+							alt="remove item"
+							className="action-icons"
+							src="https://cdn-icons-png.flaticon.com/512/1828/1828899.png"
+						/>
+
+						{/* delete cart item  */}
+						<img
+							src="https://cdn-icons-png.flaticon.com/512/1828/1828843.png "
+							alt="delete item "
+							className="action-icons "
+						/>
+					</div>
 				</div>
 			</div>
 		);
