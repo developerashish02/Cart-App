80cd30579560dec22e112200f54bcd3bde672bbf
Ashish_Amar_Gaikwad <developerashish02@gmail.com>
2022-03-18 21:02:17 +0530

    added Total price functinality

--- src/App.js
+++ src/App.js
@@ -12,21 +12,21 @@ class App extends React.Component {
 					price: 999,
 					title: "Mobail Phone",
 					qty: 1,
-					img: "",
+					img: 'https://images.unsplash.com/photo-1581795686346-b0f855c430e1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bW9iYWlsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60',
 					id: 1,
 				},
 				{
 					price: 499,
 					title: "Watch",
 					qty: 11,
-					img: "",
+					img: 'https://images.unsplash.com/photo-1522312346375-d1a52e2b99b3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8d2F0Y2h8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60',
 					id: 2,
 				},
 				{
-					price: 15000,
+					price: 1500,
 					title: "Laptop",
 					qty: 100,
-					img: "",
+					img: "https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8bGFwdG9wfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
 					id: 3,
 				},
 			],
@@ -75,13 +75,25 @@ class App extends React.Component {
 	// count total quantity 
 	getCartCount = () => {
 		const { products } = this.state;
-		let count = 0 ;
+		let count = 0;
 
 		products.forEach((product) => {
-			count += product.qty; 
+			count += product.qty;
 		})
 
-		return count; 	
+		return count;
+	}
+
+	// Total price of the cart Item 
+	getCartTotal = () => {
+		const { products } = this.state;
+
+		let Carttotal = 0;
+		products.map((product) => {
+			Carttotal = Carttotal + product.qty * product.price;
+		})
+
+		return Carttotal;
 	}
 
 	render() {
@@ -95,10 +107,26 @@ class App extends React.Component {
 					onDecreaseQuantity={this.handleDecreaseQuantity}
 					onDeleteItem={this.handleDeleteProduct}
 				/>
+
+				{/* Total price */}
+				<div style={style.total}>
+					Total:{this.getCartTotal()}
+				</div>
 			</div>
 		);
 	}
 
+}
+
+const style = {
+	total: {
+		fontSize: 25,
+		padding: 20,
+		background: 'cyan',
+		color: 'green',
+		height: 20,
+		
+	}
 }
 
 export default App;
