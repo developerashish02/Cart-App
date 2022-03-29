2580cc65498f1f146518cf2365d23a59152d126e
Ashish_Amar_Gaikwad <developerashish02@gmail.com>
2022-03-29 22:09:24 +0530

    firestore is added to store the cart items data

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyDngeZ5MNHziBYJzVr3c07qlFCCX1l0e2g",
+    authDomain: "cart-1c300.firebaseapp.com",
+    projectId: "cart-1c300",
+    storageBucket: "cart-1c300.appspot.com",
+    messagingSenderId: "478935146720",
+    appId: "1:478935146720:web:e7945ace3f15bfd255b085"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
 
 ReactDOM.render(
   <React.StrictMode>
